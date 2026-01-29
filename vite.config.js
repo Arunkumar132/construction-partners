43d639ebc55c352c2b57772e974062abad777c2b
Arunkumar132 <arun132003@gmail.com>
2026-01-29 21:32:14 +0530

    Arun

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // relative paths for Netlify
+  base: "/", // relative paths for Netlify
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
